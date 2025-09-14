d137e5b731484f701c3f1f99c6934ffb73827360
KoshiSG4 <gunasinghekoshi@gmail.com>
2025-09-15 00:02:10 +0530

    updated keycloack init options

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,25 +6,34 @@ import App from './App.js';
 import keycloak from './keycloak.js';
 import './index.css';
 
-keycloak.init({ onLoad: 'login-required' }).then((authenticated) => {
-	if (authenticated) {
-		ReactDOM.createRoot(document.getElementById('root')!).render(
-			<React.StrictMode>
-				<Provider store={store}>
-					<App />
-				</Provider>
-			</React.StrictMode>
-		);
-	} else {
-		keycloak.login();
-	}
-});
+keycloak
+	.init({
+		onLoad: 'login-required',
+		checkLoginIframe: false,
+		redirectUri: window.location.origin,
+	})
+	.then((authenticated) => {
+		if (authenticated) {
+			ReactDOM.createRoot(document.getElementById('root')!).render(
+				<React.StrictMode>
+					<Provider store={store}>
+						<App />
+					</Provider>
+				</React.StrictMode>
+			);
+		} else {
+			keycloak.login();
+		}
+	});
 
 //refresh token automatically
 setInterval(() => {
-	keycloak.updateToken(60).then((refreshed) => {
-		if (refreshed) {
-			console.log('Token Refreshed');
-		}
-	});
+	keycloak
+		.updateToken(60)
+		.then((refreshed) => {
+			if (refreshed) {
+				console.log('Token Refreshed');
+			}
+		})
+		.catch(() => keycloak.login());
 }, 1000);
