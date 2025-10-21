94fb47268641ef098adf63412a2b12b69b0eb9b4
KoshiSG4 <gunasinghekoshi@gmail.com>
2025-10-21 09:53:40 +0530

    added binary targets

--- client/src/Pages/Login.tsx
+++ client/src/Pages/Login.tsx
@@ -211,7 +211,7 @@ const Login = () => {
 										onChange={handleChange}
 										required
 										placeholder="you@example.com"
-										className={`w-full rounded-lg border border-white/10 bg-white/4 px-3 py-2 text-white placeholder-white/60 focus:outline-none focus:ring-2 focus:ring-indigo-400 ${
+										className={`w-full rounded-lg border border-white/10 bg-white/4 px-3 py-2 text-black/50 placeholder-white/60 focus:outline-none focus:ring-2 focus:ring-indigo-400 ${
 											errors.email
 												? 'ring-2 ring-red-400/60'
 												: ''
@@ -239,7 +239,7 @@ const Login = () => {
 										onChange={handleChange}
 										required
 										placeholder="Password"
-										className={`w-full rounded-lg border border-white/10 bg-white/4 px-3 py-2 text-white placeholder-white/60 focus:outline-none focus:ring-2 focus:ring-indigo-400 ${
+										className={`w-full rounded-lg border border-white/10 bg-white/4 px-3 py-2 text-black/50 placeholder-white/60 focus:outline-none focus:ring-2 focus:ring-indigo-400 ${
 											errors.password
 												? 'ring-2 ring-red-400/60'
 												: ''
